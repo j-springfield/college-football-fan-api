b7e654040d59f8d3cbdf0bfa21c5db68ea16e994
Jay Springfield <jay.springfield93@gmail.com>
2023-05-30 22:18:09 -0400

    Added the endpoint for getting team roster data by year.

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -20,8 +20,29 @@ async function getTeams(req, res) {
     }
 }
 
+async function getRoster(req, res) {
+    try {
+        if (!req.query.team || !req.query.year) {
+            throw new Error('Either the team or the year is missing from the query parameters for the team roster endpoint.');
+        }
 
+        const response = await axios.get('https://api.collegefootballdata.com/roster', {
+            headers: {
+                Authorization: `Bearer ${API_KEY}`
+            },
+            params: {
+                team: req.query.team.toString(),
+                year: req.query.year.toString()
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to make the request to get team roster data.' });
+    }
+}
 
 export default {
-    getTeams
+    getTeams,
+    getRoster
 }
--- routes/teams.js
+++ routes/teams.js
@@ -4,5 +4,6 @@ import teamsController from '../controllers/teamsController.js';
 const router = express.Router();
 
 router.get('/', teamsController.getTeams);
+router.get('/roster', teamsController.getRoster);
 
 export default router;
