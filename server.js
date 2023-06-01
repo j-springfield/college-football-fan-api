ab16199056021c2225dee88bccabc97fd5d0a48f
Jay Springfield <jay.springfield93@gmail.com>
2023-06-01 12:46:45 -0400

    For dev purposes, bypassed the CORS errors.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import teamsRoute from './routes/teams.js';
 import conferencesRoute from './routes/conferences.js';
 import coachesRoute from './routes/coaches.js';
 import rankingsRoute from './routes/rankings.js';
 
 const app = express();
+app.use(cors());
 const PORT = 8080;
 
 app.use('/api/teams', teamsRoute);
